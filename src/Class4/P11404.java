91d3c014c5d5071d3c1f9cbf27832f8d057f3142
김시진 <koreatlwls@gimsijin-ui-MacBookAir.local>
2022-06-26 15:59:15 +0900

    Feat : 220626 solved.ac class4 3problems solved (JAVA)

---
+++ src/Class4/P11404.java
@@ -0,0 +1,62 @@
+package Class4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11404 {
+
+    static int N, M;
+    static int[][] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = 987654321;
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            dist[start][end] = Math.min(dist[start][end], weight);
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (dist[i][j] == 987654321) {
+                    sb.append(0 + " ");
+                } else {
+                    sb.append(dist[i][j] + " ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
