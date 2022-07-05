6971454b82745da83e2c9c671d9141760892f7c4
김시진 <koreatlwls@gimsijin-ui-MacBookAir.local>
2022-07-05 16:52:17 +0900

    Feat : 220705 baekjoon 1problem solved (Kotlin)

---
+++ src/Implementation/P17413.kt
@@ -0,0 +1,43 @@
+package Implementation
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+
+fun main() = with(br) {
+    val input = br.readLine()
+
+    var stack = Stack<Char>()
+    var inOut = false
+
+    for(i in input.indices){
+        if(input[i] == '<'){
+            inOut = true
+
+            while(stack.isNotEmpty()){
+                print(stack.pop())
+            }
+
+            print(input[i])
+        }else if(input[i] == '>'){
+            inOut = false
+            print(input[i])
+        }else if(inOut){
+            print(input[i])
+        }else if(!inOut){
+            if(input[i] ==' '){
+                while(stack.isNotEmpty()){
+                    print(stack.pop())
+                }
+                print(input[i])
+            }else{
+                stack.push(input[i])
+            }
+        }
+    }
+    while(stack.isNotEmpty()){
+        print(stack.pop())
+    }
+}
